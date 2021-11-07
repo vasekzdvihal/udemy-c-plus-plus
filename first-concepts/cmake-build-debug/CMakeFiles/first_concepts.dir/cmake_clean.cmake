b132c08aae4ea064b63ba9644da67ca24266cf17
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-07 23:02:09 +0100

    First commit

---
+++ first-concepts/cmake-build-debug/CMakeFiles/first_concepts.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_concepts.dir/main.cpp.obj"
+  "first_concepts.exe"
+  "first_concepts.exe.manifest"
+  "first_concepts.lib"
+  "first_concepts.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_concepts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
