00ab16348529f6e650e2dfcf087005139aa16425
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-09 23:43:02 +0100

    AxeGame init

---
+++ axeGame/cmake-build-debug/CMakeFiles/axeGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/axeGame.dir/main.cpp.obj"
+  "axeGame.exe"
+  "axeGame.exe.manifest"
+  "axeGame.lib"
+  "axeGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/axeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
