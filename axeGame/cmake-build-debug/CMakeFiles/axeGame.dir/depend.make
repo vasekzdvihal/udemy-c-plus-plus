c22f62bcc6b97d7acc1211294e5dc397b5255adf
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-20 22:42:09 +0100

    Project fix

--- axeGame/cmake-build-debug/CMakeFiles/axeGame.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for axeGame.
-# This may be replaced when dependencies are built.
--- axeGame/cmake-build-debug/cmake_install.cmake
+++ axeGame/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/zdvih/source/GitHub/udemy-c-plus-plus/axeGame
+# Install script for directory: C:/Users/zdvih/Desktop/raylib-cmake-template-master
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/axeGame")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/my_raylib_game")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,6 +32,12 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/zdvih/Desktop/raylib-cmake-template-master/cmake-build-debug/_deps/raylib-build/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -40,5 +46,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/zdvih/source/GitHub/udemy-c-plus-plus/axeGame/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/zdvih/Desktop/raylib-cmake-template-master/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
